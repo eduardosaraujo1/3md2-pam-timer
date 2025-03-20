bf7687c222ef4e3435a4a8b2fe0356dae00643f9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-20 15:04:12 -0300

    fix: some stuff

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import {useState, useEffect} from 'react';
-import { View, StyleSheet, Text, Pressable, SafeAreaView } from 'react-native';
+import { useState, useEffect } from "react";
+import { View, StyleSheet, Text, Pressable, SafeAreaView } from "react-native";
 
 /**
  * Because NPM is for the weak
  * @param {string} str
  * @param {number} len
  */
-function leftPad(str, len, ch='0') {
+function leftPad(str, len, ch = "0") {
   const strLen = str.length;
 
   if (strLen < len) {
-    return ch.repeat(len-strLen) + str;
+    return ch.repeat(len - strLen) + str;
   } else {
     return str;
   }
@@ -23,62 +23,68 @@ const App = () => {
 
   useEffect(() => {
     if (status) {
-      interval = setInterval(
-        () => setCount((count) => count+1), 
-        100
-      );
+      interval = setInterval(() => setCount((count) => count + 1), 100);
     } else {
       clearInterval(interval);
     }
   }, [status]);
 
   const milisegundos = count % 10;
-  const segundos = Math.floor(count/10) % 60;
+  const segundos = Math.floor(count / 10) % 60;
   const minutos = Math.floor(count / 600);
-  const fmt = minutos > 0 
-    ? `${leftPad(minutos, 2)}:${leftPad(segundos.toString(), 2)}.${milisegundos}` 
-    : `${segundos}.${milisegundos}`;
+  const fmt =
+    minutos > 0
+      ? `${leftPad(minutos, 2)}:${leftPad(
+          segundos.toString(),
+          2
+        )}.${milisegundos}`
+      : `${segundos}.${milisegundos}`;
 
-  return <SafeAreaView style={styles.container}>
-    <Text style={styles.display}>{fmt}</Text>
-    <View style={{flexDirection: 'row', gap: 4}}>
-      <Pressable onPress={() => setStatus((status) => !status)}>
-        <Text style={{...styles.button, backgroundColor: status ? 'red' : 'green'}}>
-          {status ? 'Parar' : 'Iniciar'}
-        </Text>
-      </Pressable>
-      <Pressable onPress={() => setCount(0)}>
-        <Text style={styles.button}>
-          Redefinir
-        </Text>
-      </Pressable>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.display}>{fmt}</Text>
+      <View style={{ flexDirection: "row", marginInline: "auto" }}>
+        <Pressable onPress={() => setStatus((status) => !status)}>
+          <Text
+            style={{
+              ...styles.button,
+              backgroundColor: status ? "red" : "green",
+            }}
+          >
+            {status ? "Parar" : "Iniciar"}
+          </Text>
+        </Pressable>
+        <Pressable onPress={() => setCount(0)}>
+          <Text style={styles.button}>Redefinir</Text>
+        </Pressable>
+      </View>
     </View>
-  </SafeAreaView>;
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     padding: 12,
-    marginBlock: 64
+    marginBlock: 64,
   },
   display: {
     fontSize: 64,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   button: {
-    backgroundColor: 'gray',
-    color: 'white',
+    backgroundColor: "gray",
+    color: "white",
     borderRadius: 4,
-    width: 'max-content',
+    width: "max-content",
     minWidth: 75,
-    fontFamily: 'system-ui',
-    textAlign: 'center',
+    fontFamily: "system-ui",
+    textAlign: "center",
     paddingInline: 8,
     paddingBlock: 6,
-    marginInline: 'auto',
+    marginInline: "auto",
     marginTop: 12,
-  }
+  },
 });
 
 export default App;
